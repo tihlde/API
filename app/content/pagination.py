241ee9973a959569b5850e2acd5d5fb87d98ed33
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2020-10-31 00:18:59 +0100

    formating

--- app/content/pagination.py
+++ app/content/pagination.py
@@ -1,35 +1,32 @@
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
 from collections import OrderedDict
 
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 from rest_framework.utils.urls import remove_query_param, replace_query_param
 
 
 class BasePagination(PageNumberPagination):
-    page_size = 1
+    page_size = 25
     page_size_query_param = "None"
 
-
     def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ('count', self.page.paginator.count),
-            ('next', self.get_next_page()),
-            ('previous', self.get_previou_page()),
-            ('results', data)
-        ]))
+        return Response(
+            OrderedDict(
+                [
+                    ("count", self.page.paginator.count),
+                    ("next", self.get_next_page()),
+                    ("previous", self.get_previou_page()),
+                    ("results", data),
+                ]
+            )
+        )
 
     def get_next_page(self):
         if not self.page.has_next():
             return None
         return self.page.next_page_number()
-    
+
     def get_previou_page(self):
         if not self.page.has_previous() or self.page.previous_page_number() < 1:
             return None
         return self.page.previous_page_number()
-        
-
-    
-
-
-    
