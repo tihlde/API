8d1c557979e04c1008cd9459055a4896ba0baae4
olros <olafrosendahl@gmail.com>
2021-01-20 23:20:32 +0100

    feat(pages): set content to empty string by default

--- app/content/models/page.py
+++ app/content/models/page.py
@@ -12,7 +12,7 @@ class Page(MPTTModel, OptionalImage, BaseModel):
     Page_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=150, unique=False)
     slug = models.SlugField(max_length=50, unique=False, null=True) 
-    content = models.TextField(null=True, blank=True)
+    content = models.TextField(blank=True)
 
     class Meta:
         unique_together = ("parent", "slug")
